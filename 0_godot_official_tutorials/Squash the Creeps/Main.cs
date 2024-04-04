85789381487a2b8b834db209b32278dfab6578a1
iharuspex <nabrosok-hd@yandex.ru>
2024-04-04 10:37:28 +0700

    add squash the creeps (official 3D tutorial)

---
+++ 0_godot_official_tutorials/Squash the Creeps/Main.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	[Export]
+	public PackedScene MobScene { get; set; }
+
+	public override void _Ready()
+	{
+		GetNode<Control>("UserInterface/Retry").Hide();
+	}
+
+	private void OnMobTimerTimeout()
+	{
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+		AddChild(mob);
+
+		mob.Squashed += GetNode<ScoreLabel>("UserInterface/ScoreLabel").OnMobSquashed;
+	}
+
+	private void OnPlayerHit()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Control>("UserInterface/Retry").Show();
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_accept") && GetNode<Control>("UserInterface/Retry").Visible)
+		{
+			GetTree().ReloadCurrentScene();
+		}
+	}
+}
