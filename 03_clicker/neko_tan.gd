f5a207b3a63a052bb9eaaff369dfa35ea3a3ec9e
Artem Paraev <nabrosok-hd@yandex.ru>
2024-07-26 20:53:10 +0700

    clicker: add pulse on click

--- 03_clicker/neko_tan.gd
+++ 03_clicker/neko_tan.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var Sprite: Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,11 @@ func _process(delta):
 	pass
 
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-		if $Sprite.get_rect().has_point(to_local(event.position)):
-			print("Click detected!")
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed() :
+				$Sprite.scale = Vector2(0.69, 0.69)
+			else:
+				$Sprite.scale = Vector2(0.7, 0.7)
+				if $Sprite.get_rect().has_point(to_local(event.position)):
+					print("Click detected!")
